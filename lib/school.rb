abef245b27127e7c296d8a9445d5b7a1c411e0e7
Tabassum Hassan <tiram819@gmail.com>
2020-03-17 13:11:06 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,4 +7,13 @@ class School
   def roster
     @roster
   end 
+  
+  def add_student(name, grade)
+    @student_name = name
+    @grade = grade
+    if @roster.include?(grade) == false
+      @roster[grade] = []
+    end
+    @roster[grade] << name
+  end
     
