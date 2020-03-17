a9458a777f97dd1f9100bf62f68d9ef3bc4d50f6
Tabassum Hassan <tiram819@gmail.com>
2020-03-17 13:17:15 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,30 +1,9 @@
 class School
-  def initialize(school_name)
-    @school_name = school name 
+  def initialize(name)
+    @name = name
     @roster = {}
   end
-  
+
   def roster
     @roster
-  end 
-  
-  def add_student(name, grade)
-    @student_name = name
-    @grade = grade
-    if @roster.include?(grade) == false
-      @roster[grade] = []
-    end
-    @roster[grade] << name
   end
-  
-  def grade(grade)
-    @roster[grade]
-  end
-
-  def sort
-    @roster.each do |grade, name|
-      @roster[grade] = name.sort
-    end
-  end
-end
-    
